74f2dd0aa66b16d2228417248aa01249ec911037
mostafa hamed <Mostafa7ahmed>
2024-09-28 22:26:04 +0300

    Frist React Project

---
+++ src/HomePage.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+import Slider from './components/Slider/Slider'
+import Product from './components/Products/product'
+
+function HomePage() {
+    return (
+
+        <div>
+    
+              <Navbar/>
+              <Slider/>
+
+              <Product/>
+
+
+
+
+               <Footer/>
+
+
+
+
+        </div>
+    )
+}
+
+export default HomePage
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='bg-dark p-3 text-bg-info text-danger'>Footer</div>
+  )
+}
+
+export default Footer
