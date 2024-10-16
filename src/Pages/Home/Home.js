33e03968b52f6bc4d7a42376022083c0e47552aa
mostafa hamed <Mostafa7ahmed>
2024-10-16 22:52:44 +0300

    edit

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Slider from '../../components/Slider/Slider'
 import "./home.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
 function Home() {
   return (
     <div className='home'> 
      
-      <h2 className='p-5 bg-body-tertiary'>Home</h2>
+      <h2 className='p-5 bg-body-tertiary'> <FontAwesomeIcon icon={faHouse}  className=" text-bg-danger p-5"/> Home</h2>
 
         
     </div>
